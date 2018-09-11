bc1182c9d83c2c6b010cdaae94ea1bd3b18a47af
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2018-09-11 17:43:43 +0900

    Add main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+print "ded man:"
+ded = raw_input()
+
+print "object:"
+obj = raw_input()
+
+print ""
+print "----"
+print ""
+
+print "30日夜、果実都某所で、 :{0}: に埋もれてdedしている :{1}: が発見された。".format(obj, ded)
+
+print ""
+
+for i in range(0, 4):
+    l = ""
+    for j in range(0, 7):
+        if i is 3:
+            if j is 3:
+                l += ":{0}: ".format(ded)
+                continue
+
+        l += ":{0}: ".format(obj)
+    print l
